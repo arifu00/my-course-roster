663002d93ed608e4fda5aa6e9f07189a5674eb64
arifu00 <arifu5758@gmail.com>
2023-09-14 21:08:08 +0600

    Project Basic structure set-up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h4 className="text-3xl font-bold text-center pt-5 pb-1">
+        Course Registration
+      </h4>
+    </>
+  );
+}
+
+export default App;
